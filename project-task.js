2e7d60cc8a55e0d56aba4d93f688d833100fc0e6
Jay S <js4cap@gmail.com>
2025-04-07 15:01:05 -0500

    Added functions to project-task.js

--- project-task.js
+++ project-task.js
@@ -33,25 +33,49 @@ This activity reinforces:
 // ============================================
 
 // Script 1 - Greeting multiple users
-console.log("Welcome, Alice!");
-console.log("Welcome, Bob!");
-console.log("Welcome, Charlie!");
+function greet(name)
+{
+   return(console.log(`Welcome, ${name}!`))
+}
+
+greet("Alice");
+greet("Bob");
+greet("Charlie");
 
 // Script 2 - Sum calculation
+function calculateSum()
+{
+   let sum = 0;
+   for(let i = 0; i < arguments.length; i++)
+   {
+      sum += arguments[i];
+   }
+   return sum;
+}
 let num1 = 5, num2 = 10;
-let sum = num1 + num2;
+let sum = calculateSum(num1, num2, num3, num4)
 console.log("The sum of 5 and 10 is " + sum);
 
 // Script 3 - Product calculation
-let product = num1 * num2;
+function calculateProduct(num1, num2)
+{
+   return num1 * num2
+}
+let product = calculateProduct(num1, num2);
 console.log("The product of 5 and 10 is " + product);
 
 // Script 4 - Print names from a list
+function printNames(names)
+{
+   for (let i = 0; i < names.length; i++) 
+   {
+      console.log(names[i]);
+   }
+}
+
 let names = ["Alice", "Bob", "Charlie"];
 console.log("Names in the list:");
-for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
-}
+printNames(names)
 
 /*
 ===========================================
